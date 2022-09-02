cd44d6118585d42fe5cd4ecde2426e57817b6814
Victoria <victoriaerygina@yandex.ru>
2022-09-02 18:08:26 +0700

    Количество элементов из отрезка в массиве

---
+++ Задача_35/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 35: Задайте одномерный массив из 123 случайных чисел.
+// Найдите количество элементов массива, значения которых лежат в отрезке [10,99]. 
+
+int[] RandomArray()
+{
+    Console.Write("Введите длину массива: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+
+    int[] array = new int[number]; // int[] array = new int[123]
+    
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 1001);
+    }
+    Console.WriteLine($"Массив: [{String.Join(", ", array)}]");
+    return array;
+}
+
+void CountOfElements(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] >= 10 && array[i] <= 99)
+        {
+            count++;
+        }
+    }
+    Console.WriteLine($"Количество элементов массива в отрезке [10,99] = {count}");        
+}
+CountOfElements(RandomArray());
